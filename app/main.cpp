896d632223922c30b369f6c17aa14b259242af9f
Anna Udobnaja <udobnaja@gmail.com>
2026-05-25 23:32:47 +0300

    WEB-5: txt

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,24 @@
 int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
-    // Parse and Validate Config
+    // Try
+    //  Parse, Validate, Normalize Config (config module)
 
-    // Run HTTP Server
+    //  Run HTTP Server (app folder class)
+    //      Run TCP Server (net module)
+    //          Create, Bind, Listen, Register sockets
+    //      Run Event Loop (net module)
+    //          Runs the infinite event cycle
+    //          Waits events from Poller
+    //          Create Connection Entity
+    //          Stores Connection Entity in ConnectionRegistry
+    //          Event readable ? ->
+    //              bad/ closed/ not enough data ? skip or do some work
+    //              Connection -> get HttpRequest
+    //              Request Handler ->  make HttpResponse from HttpRequest
+    //              Serialize Response to bytes
+    //              make event ready to write
+    //          Event Writable?
+    //          write
+    // Catch
 }
