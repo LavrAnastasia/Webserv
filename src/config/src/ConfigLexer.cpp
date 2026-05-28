8e68c9df216399274477707bf6c473e53db54559
Anna Udobnaja <udobnaja@gmail.com>
2026-05-28 23:32:01 +0300

    WEB-13: Easy ConfigLexer

--- src/config/src/ConfigLexer.cpp
+++ src/config/src/ConfigLexer.cpp
@@ -1,10 +1,56 @@
 #include "ConfigLexer.hpp"
 
+#include <cctype>
+
 ConfigLexer::ConfigLexer(const std::string& source) : source_(source) {
+    //
 }
 
 std::vector<Token> ConfigLexer::run() {
-    return std::vector<Token>{Token{.type = TokenType::Word, .value = "smth"}};
+    std::vector<Token> result{};
+    const std::size_t size = source_.size();
+    for (std::size_t i = 0; i < size; ++i) {
+        if (std::isspace(static_cast<unsigned char>(source_[i]))) {
+            continue;
+        }
+
+        // skip comments
+        if (source_[i] == '#') {
+            auto pos = source_.find('\n', i);
+            if (pos == std::string::npos) {
+                break;
+            }
+            i = pos;
+            continue;
+        }
+
+        if (source_[i] == '{') {
+            result.push_back(Token{.type = TokenType::LeftBrace, .value = ""});
+            continue;
+        }
+
+        if (source_[i] == '}') {
+            result.push_back(Token{.type = TokenType::RightBrace, .value = ""});
+            continue;
+        }
+
+        if (source_[i] == ';') {
+            result.push_back(Token{.type = TokenType::Semicolon, .value = ""});
+            continue;
+        }
+
+        const std::size_t start = i;
+        const auto end = source_.find_first_of(" \t\r\n{};#", i);
+
+        if (end == std::string::npos) {
+            result.push_back(Token{.type = TokenType::Word, .value = source_.substr(start)});
+            break;
+        }
+
+        result.push_back(Token{.type = TokenType::Word, .value = source_.substr(start, end - start)});
+        i = end - 1;
+    }
+    return result;
 }
 
 std::vector<Token> ConfigLexer::tokenize(const std::string& source) {
