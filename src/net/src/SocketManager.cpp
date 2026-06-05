f1fa254790b14ee8ccf99686075f7d1202bd9ed4
Jussi Jahkola <jjahkola@c1r6p8.hive.fi>
2026-06-05 12:51:19 +0300

    WEB-19: SocketManager class complete, socket manager module first build complete.

--- src/net/src/SocketManager.cpp
+++ src/net/src/SocketManager.cpp
@@ -1,4 +1,5 @@
 #include "SocketManager.hpp"
+#include "config/ServerConfig.hpp"
 
 SocketManager::~SocketManager() {
     for (ServerSocket* server : servers_) {
@@ -7,11 +8,32 @@ SocketManager::~SocketManager() {
     servers_.clear();
 }
 
+/*
+    const std::vector<ServerConfig>& configs
+    "A read-only reference to a vector of ServerConfig objects, named configs"
+*/
+
 void SocketManager::createServers(const std::vector<ServerConfig>& configs) {
+    //1. loop through server configs
+    for (const ServerConfig& config : configs) {
+        //2. loop through listen configs in each server config
+        for (const ListenConfig& listenBlock : config.listen) {
+            //3. create socket, set it to non-blocking and save it in servers_
+            ServerSocket* newSocket = new ServerSocket(listenBlock.port);
+            newSocket->setNonBlocking();
+            servers_.push_back(newSocket);
+        }
+    }
 }
 
+/*
+    when function signature specifies return by reference, '&' not needed in return statement
+*/
+
 const std::vector<ServerSocket*>& SocketManager::getServers() const {
+    return servers_;
 }
+
 /*
     Range-based loop syntax:
     for (ServerSocket* server : servers_) {
@@ -20,5 +42,33 @@ const std::vector<ServerSocket*>& SocketManager::getServers() const {
     1. ServerSocket* server <- current item (temporary variable)
     2. : <- reads as "in"
     3. servers_ <- location of items being operated on
+
+    Write a loop to go through the configs vector (by const & so you don't copy the
+    massive structs).
+
+    Inside that loop, write another loop to go through the config.listen vector
+    (also by const &).
+
+    Inside the inner loop, you will extract the port,
+    build the socket, make it non-blocking, and save it to servers_.
+
+
+    struct ServerConfig {
+    std::vector<ListenConfig> listen;
+
+    std::filesystem::path root;
+    std::string index;
+    std::size_t clientMaxBodySize;
+
+    std::unordered_map<int, std::filesystem::path> errorPages;
+
+    std::vector<LocationConfig> locations;
+};
+
+
+    struct ListenConfig {
+    std::string host;
+    std::uint16_t port;
+};
 
 */
