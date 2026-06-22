cef1974ca4b447e89ffdff774e568a30051ca6af
Jussi Jahkola <jjahkola@c1r6p20.hive.fi>
2026-06-22 18:26:22 +0300

    WEB-9: TcpServer class definition

---
+++ src/net/include/net/TcpServer.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "SocketManager.hpp"
+#include "config/ServerConfig.hpp"
+
+#include <string>
+#include <vector>
+
+/*
+    The TCP server owns the SocketManager, sets up listening ports, and routes
+    incoming connection requests to the appropriate ServerSocket.
+*/
+
+class TcpServer {
+private:
+    /*
+    mapping of ports to FDs is handled inside the SocketManager's ServerSocket objects
+*/
+    SocketManager socketManager_;
+
+public:
+    /*
+    memory and fd cleanup is handled inside the SocketManager, ServerSocket and Socket
+    classes, so default destructor will work here
+*/
+    TcpServer() = default;
+    ~TcpServer() = default;
+
+    /*
+    Initialization: Parsed config blocks are passed to SocketManager, which creates,
+    binds and sets sockets to listen mode.
+*/
+    void setupServers(const std::vector<ServerConfig>& configs);
+
+    /*
+    Called once by EventLoop at startup, to get list of listening FDs to
+    register with Poller
+*/
+    std::vector<int> getListeningFds() const;
+
+    struct ClientInfo {
+        int fd;
+        std::string ip;
+        int port;
+    };
+    /*
+    Called by EventLoop when Poller triggers a POLLIN on a listening port.
+    Finds the corresponding ServerSocket object and tells it to accept the new client.
+    Obtains client info from the ServerSocket and returns it as a ClientInfo object.
+*/
+    ClientInfo acceptClient(int listenFd);
+};
