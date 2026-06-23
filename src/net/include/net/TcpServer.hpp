a239e947e6f3896591644b17ab6765404e605137
Jussi Jahkola <jjahkola@c3r1p3.hive.fi>
2026-06-23 19:21:34 +0300

    WEB-9: added member variable to store copy of config file, and helper function to get ServerConfig block associated with a specific server port

--- src/net/include/net/TcpServer.hpp
+++ src/net/include/net/TcpServer.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "SocketManager.hpp"
-#include "config/ServerConfig.hpp"
+#include "config/Config.hpp"
 
 #include <string>
 #include <vector>
@@ -13,40 +13,51 @@
 
 class TcpServer {
 private:
-    /*
-    mapping of ports to FDs is handled inside the SocketManager's ServerSocket objects
-*/
+    //mapping of ports to FDs is handled inside the SocketManager's ServerSocket objects
     SocketManager socketManager_;
+    //copy of Config struct stored locally
+    Config config_;
+
 
 public:
     /*
     memory and fd cleanup is handled inside the SocketManager, ServerSocket and Socket
     classes, so default destructor will work here
-*/
+    */
     TcpServer() = default;
     ~TcpServer() = default;
 
     /*
-    Initialization: Parsed config blocks are passed to SocketManager, which creates,
+    Initialization: Parsed config blocks contained in main config struct
+    are passed to SocketManager, which creates,
     binds and sets sockets to listen mode.
-*/
-    void setupServers(const std::vector<ServerConfig>& configs);
+    */
+    void setupServers(const Config& config);
 
     /*
     Called once by EventLoop at startup, to get list of listening FDs to
     register with Poller
-*/
+    */
     std::vector<int> getListeningFds() const;
+
+    /*
+    Used by EventLoop to:
+    1. check server's client_max_body_size
+    2. find root of the server's file system
+    3. check for custom error pages
+    */
+    const ServerConfig* getServerConfigByPort(int port) const;
 
     struct ClientInfo {
         int fd;
         std::string ip;
-        int port;
+        int port; // Port on client's machine
+        int serverPort; // Port on our machine (80, 443 etc)
     };
     /*
     Called by EventLoop when Poller triggers a POLLIN on a listening port.
     Finds the corresponding ServerSocket object and tells it to accept the new client.
     Obtains client info from the ServerSocket and returns it as a ClientInfo object.
-*/
+    */
     ClientInfo acceptClient(int listenFd);
 };
