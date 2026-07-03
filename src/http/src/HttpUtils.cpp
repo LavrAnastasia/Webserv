798dda77286fa1da35e3320bc6bfaaaa7dea015b
Anastasiia Lavrukhina <alavrukh@student.hive.fi>
2026-07-03 20:34:31 +0300

    WEB-22: locale independent tolower and trim

--- src/http/src/HttpUtils.cpp
+++ src/http/src/HttpUtils.cpp
@@ -4,30 +4,40 @@
 #include <cctype>
 
 namespace Http::Ascii {
+    namespace {
+        char toLowerAsciiChar(char c) {
+            unsigned char uc = static_cast<unsigned char>(c);
+
+            if (uc >= 'A' && uc <= 'Z') {
+                return static_cast<char>(uc - 'A' + 'a');
+            }
+
+            return c;
+        }
+
+        bool isWhiteSpace(char c) {
+            return c == Http::Syntax::SP || c == Http::Syntax::HTAB;
+        }
+    } // namespace
     std::string tolower(const std::string& value) {
         std::string result = value;
 
-        std::transform(result.begin(), result.end(), result.begin(), [](char c) {
-            return static_cast<char>(std::tolower(static_cast<unsigned char>(c)));
-        });
+        std::transform(result.begin(), result.end(), result.begin(), [](char c) { return toLowerAsciiChar(c); });
+
         return result;
     }
 
     std::string trim(const std::string& value) {
-        std::string::const_iterator start = std::find_if(value.begin(), value.end(), [](char c) {
-            return !std::isspace(static_cast<unsigned char>(c));
-        });
-    
+        auto start = std::find_if(value.begin(), value.end(), [](char c) { return !isWhiteSpace(c); });
+
         if (start == value.end()) {
             return "";
         }
-    
-        std::string::const_reverse_iterator reverseEnd = std::find_if(value.rbegin(), value.rend(), [](char c) {
-            return !std::isspace(static_cast<unsigned char>(c));
-        });
-    
-        std::string::const_iterator end = reverseEnd.base();
-    
+
+        auto reverseEnd = std::find_if(value.rbegin(), value.rend(), [](char c) { return !isWhiteSpace(c); });
+
+        auto end = reverseEnd.base();
+
         return std::string(start, end);
     }
 
