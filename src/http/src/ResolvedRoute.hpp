2ed16b8f371f3a0da0c171e2d872516ebc58a087
Anna Udobnaja <udobnaja@gmail.com>
2026-07-07 14:40:29 +0300

    WEB-27: ResolvedRoute struct

--- src/http/src/ResolvedRoute.hpp
+++ src/http/src/ResolvedRoute.hpp
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <filesystem>
+#include <map>
+#include <optional>
+#include <set>
+#include <string>
+
+#include "config/CgiConfig.hpp"
+#include "config/RedirectConfig.hpp"
+#include "config/UploadConfig.hpp"
+#include "http/HttpMethod.hpp"
+#include "http/HttpStatus.hpp"
+
+struct ResolvedRoute {
+    std::string locationPath;
+
+    std::filesystem::path root;
+    std::string index;
+    std::size_t clientMaxBodySize;
+
+    std::set<HttpMethod> allowedMethods;
+    bool autoindex = false;
+
+    std::optional<RedirectConfig> redirect;
+    std::optional<UploadConfig> upload;
+    std::optional<CgiConfig> cgi;
+
+    std::map<HttpStatus, std::filesystem::path> errorPages;
+};
