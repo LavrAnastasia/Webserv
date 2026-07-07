06286c00547a95459c0062d12df714c8a81d5adf
Anna Udobnaja <udobnaja@gmail.com>
2026-07-07 15:29:00 +0300

    WEB-27: resolve boilerplate

--- src/http/src/Router.cpp
+++ src/http/src/Router.cpp
@@ -1,1 +1,7 @@
 #include "Router.hpp"
+
+std::optional<ResolvedRoute> Router::resolve(const HttpRequest& request, const ServerConfig& server) const {
+    (void)request;
+    (void)server;
+    return std::nullopt;
+}
