0459ff67fbd4e2acfe63991096f37bb903a3e24e
Anastasia Lavrukhina <an.lavrukhina@gmail.com>
2026-07-21 16:12:07 +0300

    WEB-17:remove status code

--- src/http/src/HttpResponse.cpp
+++ src/http/src/HttpResponse.cpp
@@ -3,103 +3,101 @@
 #include "HttpUtils.hpp"
 #include "http/HttpResponse.hpp"
 
-namespace {
-    std::string reasonPhraseFor(int statusCode) {
-        switch (statusCode) {
-            case 100:
-                return "Continue";
-            case 101:
-                return "Switching Protocols";
+// namespace {
+//     std::string reasonPhraseFor(int statusCode) {
+//         switch (statusCode) {
+//             case 100:
+//                 return "Continue";
+//             case 101:
+//                 return "Switching Protocols";
 
-            case 200:
-                return "OK";
-            case 201:
-                return "Created";
-            case 202:
-                return "Accepted";
-            case 204:
-                return "No Content";
+//             case 200:
+//                 return "OK";
+//             case 201:
+//                 return "Created";
+//             case 202:
+//                 return "Accepted";
+//             case 204:
+//                 return "No Content";
 
-            case 300:
-                return "Multiple Choices";
-            case 301:
-                return "Moved Permanently";
-            case 302:
-                return "Found";
-            case 303:
-                return "See Other";
-            case 304:
-                return "Not Modified";
-            case 307:
-                return "Temporary Redirect";
-            case 308:
-                return "Permanent Redirect";
+//             case 300:
+//                 return "Multiple Choices";
+//             case 301:
+//                 return "Moved Permanently";
+//             case 302:
+//                 return "Found";
+//             case 303:
+//                 return "See Other";
+//             case 304:
+//                 return "Not Modified";
+//             case 307:
+//                 return "Temporary Redirect";
+//             case 308:
+//                 return "Permanent Redirect";
 
-            case 400:
-                return "Bad Request";
-            case 401:
-                return "Unauthorized";
-            case 403:
-                return "Forbidden";
-            case 404:
-                return "Not Found";
-            case 405:
-                return "Method Not Allowed";
-            case 408:
-                return "Request Timeout";
-            case 409:
-                return "Conflict";
-            case 411:
-                return "Length Required";
-            case 413:
-                return "Content Too Large";
-            case 414:
-                return "URI Too Long";
-            case 415:
-                return "Unsupported Media Type";
-            case 418:
-                return "I'm a Teapot";
-            case 422:
-                return "Unprocessable Content";
-            case 429:
-                return "Too Many Requests";
-            case 431:
-                return "Request Header Fields Too Large";
+//             case 400:
+//                 return "Bad Request";
+//             case 401:
+//                 return "Unauthorized";
+//             case 403:
+//                 return "Forbidden";
+//             case 404:
+//                 return "Not Found";
+//             case 405:
+//                 return "Method Not Allowed";
+//             case 408:
+//                 return "Request Timeout";
+//             case 409:
+//                 return "Conflict";
+//             case 411:
+//                 return "Length Required";
+//             case 413:
+//                 return "Content Too Large";
+//             case 414:
+//                 return "URI Too Long";
+//             case 415:
+//                 return "Unsupported Media Type";
+//             case 422:
+//                 return "Unprocessable Content";
+//             case 429:
+//                 return "Too Many Requests";
+//             case 431:
+//                 return "Request Header Fields Too Large";
 
-            case 500:
-                return "Internal Server Error";
-            case 501:
-                return "Not Implemented";
-            case 502:
-                return "Bad Gateway";
-            case 503:
-                return "Service Unavailable";
-            case 504:
-                return "Gateway Timeout";
-            case 505:
-                return "HTTP Version Not Supported";
+//             case 500:
+//                 return "Internal Server Error";
+//             case 501:
+//                 return "Not Implemented";
+//             case 502:
+//                 return "Bad Gateway";
+//             case 503:
+//                 return "Service Unavailable";
+//             case 504:
+//                 return "Gateway Timeout";
+//             case 505:
+//                 return "HTTP Version Not Supported";
 
-            default:
-                return "Unknown Status";
-        }
-    }
+//             default:
+//                 return "Unknown Status";
+//         }
+//     }
 
-    bool isRedirectStatus(int statusCode) {
-        return statusCode == 301 || statusCode == 302 || statusCode == 303 || statusCode == 307 || statusCode == 308;
-    }
+//     bool isRedirectStatus(int statusCode) {
+//         return statusCode == 301 || statusCode == 302 || statusCode == 303 || statusCode == 307 || statusCode == 308;
+//     }
 
-    bool isManagedHeader(const std::string& key) {
-        return key == Http::Header::ContentLength || key == Http::Header::TransferEncoding ||
-            key == Http::Header::Connection;
-    }
-} // namespace
+//     bool isManagedHeader(const std::string& key) {
+//         return key == Http::Header::ContentLength || key == Http::Header::TransferEncoding ||
+//             key == Http::Header::Connection;
+//     }
+// } // namespace
 
 void HttpResponse::setStatus(int statusCode) {
     if (statusCode < 100 || statusCode > 599) {
         throw std::invalid_argument("invalid HTTP status code");
     }
 
-    setStatus(statusCode, reasonPhraseFor(statusCode));
+    // setStatus(statusCode, reasonPhraseFor(statusCode));
 }
 
 void HttpResponse::setStatus(int statusCode, std::string reasonPhrase) {
@@ -130,12 +128,12 @@ void HttpResponse::setHeader(std::string name, std::string value) {
 
     const std::string key = Http::Ascii::tolower(name);
 
-    if (isManagedHeader(key)) {
-        throw std::invalid_argument(
-            "header is managed by HttpResponse "
-            "or HttpSerializer"
-        );
-    }
+    // if (isManagedHeader(key)) {
+    //     throw std::invalid_argument(
+    //         "header is managed by HttpResponse "
+    //         "or HttpSerializer"
+    //     );
+    // }
 
     _headers.setOrReplace(std::move(name), std::move(value));
 }
@@ -209,9 +207,9 @@ HttpResponse HttpResponse::error(int statusCode, std::string body) {
 }
 
 HttpResponse HttpResponse::redirect(int statusCode, std::string target) {
-    if (!isRedirectStatus(statusCode)) {
-        throw std::invalid_argument("unsupported redirect status code");
-    }
+    // if (!isRedirectStatus(statusCode)) {
+    //     throw std::invalid_argument("unsupported redirect status code");
+    // }
 
     HttpResponse response;
     response.setStatus(statusCode);
