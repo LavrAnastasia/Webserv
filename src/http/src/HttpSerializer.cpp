a796fe48be29c47068be004cce2c0b85905f6cb6
Anastasia Lavrukhina <an.lavrukhina@gmail.com>
2026-07-22 16:31:18 +0300

    WEB-17: more protection for Content-Length

--- src/http/src/HttpSerializer.cpp
+++ src/http/src/HttpSerializer.cpp
@@ -59,6 +59,16 @@ std::string HttpSerializer::serialize(const HttpResponse& response, bool headers
         .append("\r\n");
 
     for (const auto& [name, value] : response.headers.entries()) {
+        // if (HttpHeaders::equals(name, "Content-Length")) {   add this after rebase
+        //     continue;
+        // }
+
+        // if (HttpHeaders::equals(name, "Transfer-Encoding")) {
+        //     throw std::invalid_argument(
+        //         "transfer encoding is not supported"
+        //     );
+        // }
+
         appendHeader(output, name, value);
     }
 
@@ -70,10 +80,9 @@ std::string HttpSerializer::serialize(const HttpResponse& response, bool headers
         appendHeader(output, "Server", "webserv");
     }
 
-    if (!bodyForbidden && !response.headers.has("Content-Length")) {
+    if (!bodyForbidden) {
         appendHeader(output, "Content-Length", std::to_string(body.size()));
     }
-
     output.append("\r\n");
 
     if (!headersOnly && !bodyForbidden) {
