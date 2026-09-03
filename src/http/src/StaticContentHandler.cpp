3e0c9a262f686296dd1100d462cc8fa5a6eccbdc
Anna Udobnaja <udobnaja@gmail.com>
2026-09-03 16:42:50 +0300

    WEB-33: flat directory response

--- src/http/src/StaticContentHandler.cpp
+++ src/http/src/StaticContentHandler.cpp
@@ -36,40 +36,30 @@ namespace {
     HttpResponse directoryResponse(
         const fs::path& directoryPath, const fs::path& root, const HttpRequest& request, const ResolvedRoute& route
     ) {
-        if (request.path.empty()) {
-            return ErrorResponseFactory::create(HttpStatus::BadRequest, route);
-        }
-
         if (request.path.back() != Http::Syntax::PathPrefix) {
             return createRedirectResponse(request);
         }
 
         if (!route.index.empty()) {
-            std::error_code error;
+            try {
+                const fs::path indexPath = fs::weakly_canonical(directoryPath / route.index);
 
-            const fs::path indexPath = fs::weakly_canonical(directoryPath / route.index, error);
-
-            if (error) {
-                const HttpStatus status = Http::Status::from(error);
-                if (status != HttpStatus::NotFound) {
-                    return ErrorResponseFactory::create(status, route);
-                }
-            } else {
                 if (!Fs::isPrefixOf(root, indexPath)) {
                     return ErrorResponseFactory::create(HttpStatus::Forbidden, route);
                 }
 
-                const fs::file_status indexStatus = fs::status(indexPath, error);
+                const fs::file_status indexStatus = fs::status(indexPath);
 
-                if (error) {
-                    const HttpStatus status = Http::Status::from(error);
-                    if (status != HttpStatus::NotFound) {
-                        return ErrorResponseFactory::create(status, route);
-                    }
-                } else if (fs::is_regular_file(indexStatus)) {
+                if (fs::is_regular_file(indexStatus)) {
                     return RegularResponseFactory::create(indexPath, route);
-                } else if (fs::exists(indexStatus)) {
+                }
+
+                if (fs::exists(indexStatus)) {
                     return ErrorResponseFactory::create(HttpStatus::Forbidden, route);
+                }
+            } catch (const fs::filesystem_error& error) {
+                if (Http::Status::from(error.code()) != HttpStatus::NotFound) {
+                    throw;
                 }
             }
         }
@@ -77,6 +67,7 @@ namespace {
         if (route.autoindex) {
             return AutoindexResponseFactory::create(directoryPath, request, route);
         }
+
         return ErrorResponseFactory::create(HttpStatus::Forbidden, route);
     }
 } // namespace
